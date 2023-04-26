8ecfb07761c5d92fdc0484e558c827928503f2d5
sgmabba2 <m.abba-shariff@liverpool.ac.uk>
2023-04-26 17:44:27 +0100

    Login Screen UI

---
+++ lib/features/authentication/presentation/forms/login_form.dart
@@ -0,0 +1,236 @@
+import 'package:business_bosses_v2/common/widgets/text_widget.dart'
+    show TextWidget;
+import 'package:country_picker/country_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../../common/ui/buttons/icon_text_button.dart';
+import '../../../../common/ui/my_outlined_button.dart';
+import '../../../../utils/theme/theme.dart';
+import '../../../../utils/validators/phone_input.dart';
+import '../../../../utils/validators/validator.dart';
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  bool _isProcessing = false;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  late AutovalidateMode _autoValidateMode = AutovalidateMode.disabled;
+  bool isEmailAuth = true;
+  String? _authCred, _password;
+  bool _invisiblePassword = true;
+  String countryCode = '+447';
+
+  onChangeCountry(Country value) {
+    List spl = value.displayName.toString().split(' ');
+    setState(() {
+      countryCode = spl[spl.length - 1].toString().split('[')[1].split(']')[0];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AbsorbPointer(
+      absorbing: _isProcessing,
+      child: Form(
+        key: _formKey,
+        autovalidateMode: _autoValidateMode,
+        child: Column(
+          children: [
+            const SizedBox(height: 25.0),
+
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextWidget(
+                  text: isEmailAuth ? 'Email' : 'Phone',
+                  size: 0,
+                  fontWeight: FontWeight.w700,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                if (isEmailAuth)
+                  TextFormField(
+                    onChanged: (String val) {
+                      _authCred = val;
+                      setState(() {});
+                    },
+                    textInputAction: TextInputAction.next,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: inputDecoration.copyWith(
+                      hintText: 'Enter your email',
+                      hintStyle: const TextStyle(
+                        color: iconColor,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      filled: true,
+                      fillColor: const Color(0xffF4F4F4),
+                    ),
+                    validator: Validator.emailValidator,
+                  )
+                else
+                  PhoneNumberInput(
+                    onChangeCountry: onChangeCountry,
+                    countryCode: countryCode,
+                    onChangeText: (String value) {
+                      _authCred = value;
+                    },
+                  )
+              ],
+            ),
+            //field user name or email
+            const SizedBox(height: 25.0),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextFormField(
+                  onChanged: (String val) {
+                    _password = val;
+                    setState(() {});
+                  },
+                  validator: Validator.passwordValidator,
+                  textInputAction: TextInputAction.done,
+                  obscureText: _invisiblePassword,
+                  keyboardType: TextInputType.visiblePassword,
+                  decoration: inputDecoration.copyWith(
+                    hintText: 'Enter your password',
+                    suffixIcon: showHideIcon(),
+                    hintStyle: const TextStyle(
+                      color: iconColor,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    filled: true,
+                    fillColor: const Color(0xffF4F4F4),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 30.0),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                width: double.infinity,
+                alignment: Alignment.centerRight,
+                child: Text('Forgot Password?',
+                    style: headline6.copyWith(
+                        fontWeight: FontWeight.bold, fontSize: 14)),
+              ),
+            ),
+            const SizedBox(height: 30.0),
+
+            MCustomButton(
+              margin: const EdgeInsets.all(2.0),
+              label: 'Login',
+              onPressed: () {},
+              isProcessing: _isProcessing,
+              buttonType: ButtonType.elevated,
+              child: Container(),
+            ),
+            const SizedBox(height: 20.0),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(child: Container(color: hintColor, height: 0.8)),
+                const SizedBox(width: 16.0),
+                RichText(
+                  text: const TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'Or',
+                        style: TextStyle(
+                          color: hintColor,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(width: 16.0),
+                Expanded(child: Container(color: hintColor, height: 0.8)),
+              ],
+            ),
+            const SizedBox(height: 20.0),
+            OutlinedButton(
+              onPressed: () {},
+              child: IconTextButton(
+                backgroundColor: Colors.transparent,
+                label: 'Sign in with Google',
+                labelColor: textColor,
+                onPressed: () async {},
+                borderRadius: BorderRadius.circular(20.0),
+                icon: Icons.search,
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            // if (Platform.isIOS)
+            //   SignInWithAppleButton(
+            //     onPressed: () async {
+            //       final MyResponse res = await MyFirebase().signInWithApple();
+            //       // print(res.);
+            //       if (res.success) {
+            //         if (res.message == "newUser") {
+            //           MyUser user = MyUser(
+            //             username: res.data.user.displayName,
+            //             uid: _firebase.uid,
+            //             email: res.data.user.email,
+            //             timestamp: DateTime.now().millisecondsSinceEpoch,
+            //             deviceTokens:
+            //                 _deviceToken != null ? [_deviceToken] : [],
+            //           );
+            //           _onJoinedDefault(_firebase.uid);
+            //           _createUser(user);
+            //         } else {
+            //           _checkProfile();
+            //         }
+            //       } else {
+            //         debugPrint("===========>>> ${res.message}");
+            //         showSnackBar(
+            //           context,
+            //           message:
+            //               'OOPS! Something went wrong. Check internet connection and try again.',
+            //         );
+            //         setState(() {
+            //           _isProcessing = false;
+            //         });
+            //       }
+            //     },
+            //   ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget showHideIcon() {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _invisiblePassword = !_invisiblePassword;
+        });
+      },
+      child: Container(
+        height: 40.0,
+        width: 40.0,
+        padding: const EdgeInsets.symmetric(vertical: 12),
+        margin: const EdgeInsets.only(right: 10),
+        child: SvgPicture.asset(
+          _invisiblePassword
+              ? 'assets/svgs/private.svg'
+              : 'assets/svgs/eye.svg',
+          // ignore: deprecated_member_use
+          color: hintColor,
+        ),
+      ),
+    );
+  }
+}
--- lib/features/authentication/presentation/login_screen.dart
+++ lib/features/authentication/presentation/login_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import '../../../common/widgets/text_widget.dart';
+import '../../../utils/theme/theme.dart';
+import '../../../navigation/routes.dart';
+import 'forms/login_form.dart';
+
 /// LOGIN SCREEEN
 class LoginScreen extends StatelessWidget {
   /// LOGIN SCREEN CONSTRUCTOR
@@ -7,8 +12,84 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(child: Text('Yooooooooo')),
+    return GestureDetector(
+      onTap: () => unFocusKeyboard(context),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 69.0),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      children: const [
+                        TextWidget(
+                          text: 'Log In',
+                          color: primaryColorLT,
+                          fontWeight: FontWeight.w700,
+                          size: 18,
+                        ),
+                        SizedBox(
+                          height: 6,
+                        ),
+                        CircleAvatar(
+                          backgroundColor: primaryColorLT,
+                          radius: 3,
+                        )
+                      ],
+                    ),
+                    const SizedBox(width: 46),
+                    Column(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context, Routes.registration as Route<Object?>);
+                          },
+                          child: const TextWidget(
+                            text: 'Sign Up',
+                            color: iconColor,
+                            fontWeight: FontWeight.w600,
+                            size: 18,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+
+                const SizedBox(
+                  height: 29,
+                ),
+
+                // Text(
+                //   'Login',
+                //   style: Theme.of(context).textTheme.headline6.copyWith(
+                //         fontWeight: FontWeight.w800,
+                //       ),
+                // ),
+                //field user name or email
+
+                const LoginForm(),
+                const SizedBox(height: 20.0),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
+  }
+
+  /// Remove keyboard focus
+  void unFocusKeyboard(BuildContext context) {
+    FocusScopeNode currentFocus = FocusScope.of(context);
+    if (!currentFocus.hasPrimaryFocus) {
+      currentFocus.unfocus();
+    }
   }
 }
