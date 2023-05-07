0e0cac74ef14bf5478922d25da54ed3d5aee07ab
wadaskid <79060645+wadaskid@users.noreply.github.com>
2023-05-07 12:36:02 +0100

    UpdatedApi service

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -57,7 +57,7 @@ class ApiService {
       headers: {'Content-Type': 'application/json'},
       body: jsonEncode(data),
     );
-    if (response.statusCode == 200) {
+    if (response.statusCode == 201) {
       final dynamic jsonResponse = json.decode(response.body);
       sandBox.write(
           Constants.ACCESS_TOKEN, jsonResponse['data']['accessToken']);
@@ -95,25 +95,6 @@ class ApiService {
       return jsonResponse;
     }
   }
-  // Future<dynamic> login(String email, String password) async {
-  //   final http.Response response = await http.post(
-  //     Uri.parse('$baseUrl/auth/sign-in'),
-  //     headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-  //     body: <String, String>{'email': email, 'password': password},
-  //   );
-  //   if (response.statusCode == 200) {
-  //     final jsonResponse = json.decode(response.body);
-  //     sandBox.write(
-  //         Constants.ACCESS_TOKEN, jsonResponse['data']['accessToken']);
-  //     Get.toNamed(Routes.bottomNavigation);
-  //     debugPrint(sandBox.read(Constants.ACCESS_TOKEN));
-  //     return jsonResponse;
-  //   } else {
-  //     final jsonResponse = response.body;
-  //     debugPrint(jsonResponse);
-  //     return;
-  //   }
-  // }
 
   ///LOGOUT
   Future<void> logout() async {
