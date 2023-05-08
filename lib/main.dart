e1c79da4edb98c6c3fd21953ea270fce345f77c3
Ernest Awuku Junior <awukuernest@gmail.com>
2023-05-08 18:58:29 +0000

    Added image count and updated sizecheck to 10mb

--- lib/main.dart
+++ lib/main.dart
@@ -52,7 +52,7 @@ class MyApp extends StatelessWidget {
               AnalyticsServices.getAnalyticObserver()
             ],
             initialRoute: userId == '' || userId == null
-                ? Routes.login
+                ? Routes.bottomNavigation
                 : Routes.bottomNavigation,
 
             // initialRoute: Routes.updateProfile,
