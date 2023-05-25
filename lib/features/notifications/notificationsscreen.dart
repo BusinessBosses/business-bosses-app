7f9ea5cef73220bc079a35865178c4add79ac99c
Ernest Awuku Junior <awukuernest@gmail.com>
2023-05-25 09:02:19 +0000

    notifications screen

--- lib/features/notifications/notificationsscreen.dart
+++ lib/features/notifications/notificationsscreen.dart
@@ -1,9 +1,13 @@
+import 'package:business_bosses_v2/common/models/quote.dart';
+import 'package:business_bosses_v2/features/notifications/widgets/nonotificationfoundwidget.dart';
+import 'package:business_bosses_v2/features/notifications/widgets/notification_item.dart';
+import 'package:business_bosses_v2/features/notifications/widgets/quotewidget.dart';
 import 'package:business_bosses_v2/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sticky_header/flutter_sticky_header.dart';
 import 'package:flutter_svg/svg.dart';
-
-import '../../common/widgets/safety_model.dart';
+import '../../utils/time_format.dart';
+import 'models/my_notification.dart';
 
 class NotificationsScreen extends StatefulWidget {
   static const String routeName = '/notifications-screen';
@@ -19,6 +23,7 @@ class _NotificationsScreenState extends State<NotificationsScreen> {
   bool isLoad = false;
   int notificationTS = DateTime.now().millisecondsSinceEpoch;
   late ScrollController _scrollController;
+  List<MyNotification> _notifications = [];
 
   @override
   void initState() {
@@ -32,18 +37,18 @@ class _NotificationsScreenState extends State<NotificationsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: SvgPicture.asset('assets/svgs/backbutton.svg'),
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: SvgPicture.asset('assets/svgs/backbutton.svg'),
+          ),
+          automaticallyImplyLeading: false,
+          title: const Text('Notifications'),
         ),
-        automaticallyImplyLeading: false,
-        title: const Text('Notifications'),
-      ),
-      body: NestedScrollView(
+        body: NestedScrollView(
           controller: _scrollController,
           headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
             return <Widget>[
@@ -54,34 +59,83 @@ class _NotificationsScreenState extends State<NotificationsScreen> {
                     padding: const EdgeInsets.all(8.0),
                     margin: const EdgeInsets.symmetric(vertical: 8.0),
                     child: // your child widget(s) go here
-                        Container()),
+                        QuoteWidget(Quote(
+                            id: 'wkwl',
+                            by: 'me',
+                            message:
+                                'kmkswdjlkjlokjpokopwjoewwjkdmljwmifjwjd.kjmlwkjd.eok',
+                            expireIn: 12233344,
+                            timestamp: 3989849345))),
               ),
             ];
           },
-          body: noNotificationsFoundWidget('notifications')),
-    );
+          body: _notifications.isEmpty
+              ? noNotificationsFoundWidget('notifications')
+              : Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 18),
+                      child: Text(
+                        'Activity',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 22),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Expanded(
+                      child: ListView.separated(
+                        // padding: EdgeInsets.all(8.0),
+                        itemCount: _notifications.length,
+                        itemBuilder: (BuildContext context, int i) {
+                          String formattedDate = TimeFormat.toDayFormat(
+                              _notifications[i].timestamp!);
+                          return (notificationsTypeIndex == 0 &&
+                                  _notifications[i].notificationType != 'chat')
+                              ? Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    if (i == 0 ||
+                                        formattedDate !=
+                                            TimeFormat.toDayFormat(
+                                              _notifications[i - 1].timestamp!,
+                                            ))
+                                      Container(
+                                        padding:
+                                            const EdgeInsets.only(left: 16.0),
+                                        child: Text(
+                                          formattedDate,
+                                          style: bodyText1,
+                                        ),
+                                      ),
+                                    NotificationItem(
+                                      _notifications[i],
+                                      onTap: () {},
+                                    )
+                                  ],
+                                )
+                              : Container();
+                        },
+                        separatorBuilder: (_, __) => const Divider(
+                            height: 0.0, indent: 12.0, endIndent: 12.0),
+                      ),
+                    ),
+                    if (isLoad)
+                      const Center(
+                        child: SizedBox(
+                          height: 20,
+                          width: 20,
+                          child: CircularProgressIndicator(),
+                        ),
+                      )
+                  ],
+                ),
+        ));
   }
-}
-
-Center noNotificationsFoundWidget(String title) {
-  return Center(
-    child: SingleChildScrollView(
-      child: Column(
-        children: [
-          SafetyModel(
-            isLoading: false,
-            icon: SvgPicture.asset(
-              'assets/svgs/notification.svg',
-              color: iconColor,
-              height: 80.0,
-              width: 80.0,
-            ),
-            title: 'You have no notification',
-            subTitle: 'You\'ll receive all new $title here',
-          ),
-          const SizedBox(height: 150.0)
-        ],
-      ),
-    ),
-  );
 }
