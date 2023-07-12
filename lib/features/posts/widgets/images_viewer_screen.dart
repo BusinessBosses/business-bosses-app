b4012da1802ee77142b142095f1906792af0cf39
WadaSKID <wadaskid@gmail.com>
2023-07-12 09:40:57 +0100

    Fixes

--- lib/features/posts/widgets/images_viewer_screen.dart
+++ lib/features/posts/widgets/images_viewer_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: public_member_api_docs
+
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper_plus/flutter_swiper_plus.dart';
