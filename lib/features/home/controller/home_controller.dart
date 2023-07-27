571934854981bdf90880a398b7f3a5690a002a13
WadaSKID <wadaskid@gmail.com>
2023-07-27 20:16:57 +0100

    Update Coins

--- lib/features/home/controller/home_controller.dart
+++ lib/features/home/controller/home_controller.dart
@@ -391,19 +391,18 @@ class HomeController extends GetxController {
     int currentTimestamp = DateTime.now().millisecondsSinceEpoch;
     int dataTime = profileController.myProfile.bossOfTheWeekUpTimeStamp ?? 0;
     int lastExecutionTimestamp = sandBox.read('lastExecutionTimestamp') ?? 0;
-    if ((profileController.myProfile.bossOfTheWeekTimeStamp == null ||
-            (currentTimestamp - dataTime >= 5271168000)) &&
-        profileController.myProfile.isUpdated == true) {
+    if (profileController.myProfile.isUpdated == true) {
       ApiService.put(
         path: 'users/${profileController.myProfile.uid}',
         body: <String, dynamic>{
           'coinscount': profileController.myProfile.coinscount! + 100,
           'bossOfTheWeekUpTimeStamp': currentTimestamp,
+          'isUpdated': false,
         },
       );
       profileController.myProfile = UserModel.fromMap({
         ...profileController.myProfile.toMap(),
-        'bossOfTheWeekUpTimeStamp': currentTimestamp
+        'isUpdated': false
       });
       sandBox.write('lastExecutionTimestamp', currentTimestamp);
       profileController.updateCoinCount(100);
