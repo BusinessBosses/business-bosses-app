e7715baf70fe8a3342bcee213b7cbdb5117cc43c
WadaSKID <wadaskid@gmail.com>
2023-08-25 05:42:25 +0100

    Update

--- lib/features/home/controller/home_controller.dart
+++ lib/features/home/controller/home_controller.dart
@@ -499,12 +499,12 @@ class HomeController extends GetxController {
       addCoinDaily();
       if (partner.data['count'] > 0) {
         bossUp?.addAll(partner.data['rows'].cast<Map<String, dynamic>>());
-        bossUp?.removeWhere((item) => item['id'] == 5);
         // Find the item with id = 5
         final Map<String, dynamic> getTitle =
             bossUp!.firstWhere((Map<String, dynamic> item) => item['id'] == 5);
 
         bossUpTitle = getTitle['companyName'];
+        bossUp?.removeWhere((item) => item['id'] == 5);
       }
     } else {
       error(true);
