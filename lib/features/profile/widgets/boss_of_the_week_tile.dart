9a7ed318fa881abf2786e36629aec91b79ab3fdf
WadaSKID <wadaskid@gmail.com>
2023-09-20 22:07:45 +0100

    Update

--- lib/features/profile/widgets/boss_of_the_week_tile.dart
+++ lib/features/profile/widgets/boss_of_the_week_tile.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:business_bosses_v2/common/models/api_response_model.dart';
 import 'package:business_bosses_v2/common/models/user_model.dart';
 import 'package:flutter/material.dart';
