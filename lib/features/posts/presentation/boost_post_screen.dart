958251b24b439d64c12eaae650b014db8d30bdfe
WadaSKID <wadaskid@gmail.com>
2023-09-28 16:13:01 +0100

    Fix To Boost Post

--- lib/features/posts/presentation/boost_post_screen.dart
+++ lib/features/posts/presentation/boost_post_screen.dart
@@ -6,7 +6,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart';
 import 'package:http/http.dart' as http;
 
 import '../../../action/action.dart';
