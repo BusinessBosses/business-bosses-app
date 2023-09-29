973aef19a5afb1c667cab52b5448250cdafc21de
Ernest Awuku Junior <awukuernest@gmail.com>
2023-09-29 18:52:55 +0000

    Update create_post_controller.dart

--- lib/features/posts/controllers/create_post_controller.dart
+++ lib/features/posts/controllers/create_post_controller.dart
@@ -38,7 +38,8 @@ class CreatePostController extends GetxController {
   bool validateCreatePostData(Map<String, dynamic> data) {
     final String title = data['title'].toString();
 
-    final RegExp invisibleCharacterPattern = RegExp(r'[^\s\w#]');
+    final RegExp invisibleCharacterPattern =
+        RegExp(r'[\p{Z}\p{C}]', unicode: true);
 
     if (invisibleCharacterPattern.hasMatch(title)) {
       return false;
