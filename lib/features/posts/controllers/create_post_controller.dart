43582d1b49f8bdedaf0f9cb60e8b4e36427d20c9
Ernest Awuku Junior <awukuernest@gmail.com>
2023-09-29 18:56:31 +0000

    Update create_post_controller.dart

--- lib/features/posts/controllers/create_post_controller.dart
+++ lib/features/posts/controllers/create_post_controller.dart
@@ -38,12 +38,12 @@ class CreatePostController extends GetxController {
   bool validateCreatePostData(Map<String, dynamic> data) {
     final String title = data['title'].toString();
 
-    final RegExp invisibleCharacterPattern =
-        RegExp(r'[\p{Z}\p{C}]', unicode: true);
+    // final RegExp invisibleCharacterPattern =
+    //     RegExp(r'[\p{Z}\p{C}]', unicode: true);
 
-    if (invisibleCharacterPattern.hasMatch(title)) {
-      return false;
-    }
+    // if (invisibleCharacterPattern.hasMatch(title)) {
+    //   return false;
+    // }
     if (title.isEmpty && imageFileList.isEmpty) {
       return false;
     } else {
