e842ef7043259559eaec5d4049fd5106b2f162f1
Ernest Awuku Junior <awukuernest@gmail.com>
2023-10-17 17:30:03 +0000

    Update home_screen.dart

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -3,7 +3,7 @@
 import 'package:business_bosses_v2/common/widgets/safety_model.dart';
 import 'package:business_bosses_v2/features/forum/controller/bossup_controller.dart';
 import 'package:business_bosses_v2/features/home/widgets/bottom_bar.dart';
-import 'package:business_bosses_v2/navigation/routes.dart';
+import 'package:business_bosses_v2/features/live_event/presentation/live_event.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
@@ -289,11 +289,19 @@ class _HomeScreenState extends State<HomeScreen> with WidgetsBindingObserver {
                                                                     MaterialStateProperty.all<
                                                                             Color>(
                                                                         Colors
-                                                                            .white),
+                                                                            .grey
+                                                                            .shade300),
                                                               ),
-                                                              onPressed: () => Get
-                                                                  .toNamed(Routes
-                                                                      .liveEvents),
+                                                              onPressed: () =>
+                                                                  Navigator
+                                                                      .push(
+                                                                context,
+                                                                MaterialPageRoute(
+                                                                  builder: (BuildContext
+                                                                          context) =>
+                                                                      const LiveEvent(),
+                                                                ),
+                                                              ),
                                                               child: const Text(
                                                                 'Live Events',
                                                                 style:
@@ -301,8 +309,8 @@ class _HomeScreenState extends State<HomeScreen> with WidgetsBindingObserver {
                                                                   fontWeight:
                                                                       FontWeight
                                                                           .bold,
-                                                                  color:
-                                                                      primaryColorLT,
+                                                                  color: Colors
+                                                                      .black,
                                                                 ),
                                                               ),
                                                             )),
@@ -404,9 +412,10 @@ class _HomeScreenState extends State<HomeScreen> with WidgetsBindingObserver {
                                               if (sponsoredIndex <
                                                   controller
                                                       .sponsoredPosts.length) {
-                                                final PostModel promotedPosts =
+                                                final promotedPosts =
                                                     controller.sponsoredPosts[
-                                                        sponsoredIndex]['data'];
+                                                            sponsoredIndex]
+                                                        ['data'] as PostModel;
                                                 final bool hasIncrementedView =
                                                     controller
                                                         .itemsWithIncrementedViews
