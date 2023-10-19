8868df07197aa54b2beb263ce248b486b1adf33b
Ernest Awuku Junior <awukuernest@gmail.com>
2023-10-19 22:40:58 +0000

    added boost post design and video design

--- lib/features/posts/widgets/all_images_item.dart
+++ lib/features/posts/widgets/all_images_item.dart
@@ -61,29 +61,44 @@ class AllImagesItem extends StatelessWidget {
                 ),
               ),
             ),
-            if (isVideo)
-              Positioned(
-                top: 0,
-                bottom: 0,
-                right: 0,
-                left: 0,
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (BuildContext context) => VideoScreen(
-                          post: post!,
-                        ),
+            Positioned(
+              top: 0,
+              bottom: 0,
+              right: 0,
+              left: 0,
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (BuildContext context) => VideoScreen(
+                        post: post!,
                       ),
-                    );
-                  },
-                  child: Icon(
-                    Icons.play_circle_outlined,
-                    color: Colors.white.withOpacity(.8),
-                    size: 48,
-                  ),
+                    ),
+                  );
+                },
+                child: Icon(
+                  Icons.play_circle_outlined,
+                  color: Colors.black.withOpacity(0.5),
+                  size: 48,
                 ),
-              )
+              ),
+            ),
+            Positioned(
+              bottom: 10,
+              right: 10,
+              child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(0.5),
+                      borderRadius: BorderRadius.circular(6)),
+                  child: const Padding(
+                    padding:
+                        EdgeInsets.only(top: 3.0, bottom: 3, left: 6, right: 6),
+                    child: Text(
+                      'time',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  )),
+            )
           ],
         ),
         // SizedBox(height: 4.0),
