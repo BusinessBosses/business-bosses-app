11f32e439beb40e441c26dcde82477a8fd0c64c6
WadaSKID <wadaskid@gmail.com>
2023-11-13 17:32:15 +0100

    Live event bug

--- lib/features/live_event/presentation/create_event.dart
+++ lib/features/live_event/presentation/create_event.dart
@@ -359,7 +359,7 @@ class _CreateEventState extends State<CreateEvent> {
                     'host': profileController.myProfile.name,
                     'photourl': profileController.myProfile.photoUrl,
                     'user': profileController.myProfile.toString(),
-                    'image': _selectedImage ?? "",
+                    'image': imageUrl ?? '',
                   };
 
                   String? jsonData = jsonEncode(dataa);
