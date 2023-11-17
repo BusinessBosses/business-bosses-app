963e8596976e95b4aac7f87bbbe440ec6f117910
WadaSKID <wadaskid@gmail.com>
2023-11-17 21:16:13 +0100

    Bug Fixes

--- lib/features/home/widgets/bottom_bar.dart
+++ lib/features/home/widgets/bottom_bar.dart
@@ -1,5 +1,4 @@
 import 'package:business_bosses_v2/features/home/bottom_nav.dart';
-import 'package:business_bosses_v2/features/live_event/presentation/create_event.dart';
 import 'package:business_bosses_v2/navigation/routes.dart';
 import 'package:business_bosses_v2/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
