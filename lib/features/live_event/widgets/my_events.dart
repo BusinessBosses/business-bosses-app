edfd866864bc7eb44ab3bbbeef5f9f6e177a2965
Ernest Junior Awuku <awukuernest@gmail.com>
2024-02-10 13:14:50 +0000

    remaining updates

--- lib/features/live_event/widgets/my_events.dart
+++ lib/features/live_event/widgets/my_events.dart
@@ -2,6 +2,7 @@ import 'package:business_bosses_v2/features/live_event/controller/live_event_con
 import 'package:business_bosses_v2/features/live_event/models/events_model.dart';
 import 'package:business_bosses_v2/features/live_event/presentation/live_event.dart';
 import 'package:business_bosses_v2/features/live_event/widgets/event_item.dart';
+import 'package:business_bosses_v2/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
@@ -18,10 +19,21 @@ class _MyEventsState extends State<MyEvents> {
   final LiveController liveController = Get.put(LiveController());
   @override
   Widget build(BuildContext context) {
+    String? previousscreen = Get.previousRoute;
     return Scaffold(
         appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: SvgPicture.asset('assets/svgs/backbutton.svg'),
+          ),
+          title: const Text(
+            'My Events',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20),
+          ),
           centerTitle: true,
-          title: const Text('My Events'),
           actions: <Widget>[
             widget.toHome != null && widget.toHome == true
                 ? IconButton(
@@ -57,11 +69,62 @@ class _MyEventsState extends State<MyEvents> {
                         ),
                       ],
                     )
-                  : const Center(
-                      child: Text(
-                        'You Have Not Chose To Attend Any Event!',
-                      ),
-                    ),
+                  : previousscreen == '/myProfileScreen'
+                      ? Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text(
+                                'You Have Not Chosen To Attend Any Event!',
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Stack(
+                                children: <Widget>[
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      color: backgroundcolorinterface,
+                                      borderRadius: BorderRadius.circular(20),
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Wrap(
+                                        crossAxisAlignment:
+                                            WrapCrossAlignment.center,
+                                        children: <Widget>[
+                                          GestureDetector(
+                                            onTap: () {
+                                              Get.to(() => const LiveEvent());
+                                            },
+                                            child: const Text(
+                                              'Go to Events',
+                                              style: TextStyle(
+                                                color: primaryColorLT,
+                                                fontSize: 13,
+                                                fontWeight: FontWeight.w700,
+                                              ),
+                                            ),
+                                          ),
+                                          const SizedBox(
+                                            width: 10,
+                                          ),
+                                          SvgPicture.asset(
+                                              'assets/svgs/nexticon.svg'),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        )
+                      : const Center(
+                          child: Text(
+                            'You Have Not Chosen To Attend Any Event!',
+                          ),
+                        ),
         ));
   }
 }
