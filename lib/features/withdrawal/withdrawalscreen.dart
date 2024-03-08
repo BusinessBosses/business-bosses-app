d34d7d9265a870babda28741d9740a21938cb9e7
Ernest Junior Awuku <awukuernest@gmail.com>
2024-03-08 12:37:16 +0000

    added withdrawal scren

--- lib/features/withdrawal/withdrawalscreen.dart
+++ lib/features/withdrawal/withdrawalscreen.dart
@@ -1,7 +1,11 @@
+import 'package:business_bosses_v2/common/widgets/buttons/custom_button.dart';
 import 'package:business_bosses_v2/features/profile/controller/profile_controller.dart';
+import 'package:business_bosses_v2/features/withdrawal/widgets/withdrawal_header_item.dart';
+import 'package:business_bosses_v2/features/withdrawal/widgets/withdrawal_item.dart';
 import 'package:business_bosses_v2/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_sticky_header/flutter_sticky_header.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 
@@ -19,8 +23,10 @@ class WithdrawalScreen extends StatefulWidget {
 }
 
 class _WithdrawalScreenState extends State<WithdrawalScreen> {
+  final ScrollController scrollController = ScrollController();
   late String _referralId;
   final ProfileController _profileController = Get.find();
+  String? _paymentmethods;
 
   @override
   void initState() {
@@ -33,101 +39,225 @@ class _WithdrawalScreenState extends State<WithdrawalScreen> {
   Widget build(BuildContext context) {
     ProfileController profileController = Get.find();
     return Scaffold(
-        backgroundColor: Colors.white,
         appBar: AppBar(
-          leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: SvgPicture.asset('assets/svgs/backbutton.svg'),
-          ),
-          centerTitle: true,
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                  padding: const EdgeInsets.only(
-                      left: 8, right: 8, top: 5, bottom: 5),
-                  decoration: BoxDecoration(
-                    color: backgroundcolorinterface,
-                    borderRadius: BorderRadius.circular(50),
+            leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: SvgPicture.asset('assets/svgs/backbutton.svg'),
+            ),
+            centerTitle: true,
+            title: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                      padding: const EdgeInsets.only(
+                          left: 8, right: 8, top: 5, bottom: 5),
+                      decoration: BoxDecoration(
+                        color: backgroundcolorinterface,
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          const Text(
+                            'My Coin Balance',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w600, fontSize: 14),
+                          ),
+                          const SizedBox(
+                            width: 2,
+                          ),
+                          SvgPicture.asset(
+                            'assets/svgs/coin.svg',
+                            height: 30,
+                          ),
+                          const SizedBox(
+                            width: 2,
+                          ),
+                          Text(
+                            '${_profileController.myProfile.coinscount ?? 0}',
+                            style: const TextStyle(
+                              color: textColor,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      )),
+                  const SizedBox(
+                    width: 30,
                   ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      const Text(
-                        'My Coin Balance',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w600, fontSize: 14),
+                ])),
+        backgroundColor: Colors.white,
+        body: NestedScrollView(
+          controller: scrollController,
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return <Widget>[
+              SliverStickyHeader(
+                sticky: false,
+                header: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 20.0, vertical: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            'Withdraw',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                          SvgPicture.asset('assets/svgs/help.svg')
+                        ],
                       ),
                       const SizedBox(
-                        width: 2,
+                        height: 50,
                       ),
-                      SvgPicture.asset(
-                        'assets/svgs/coin.svg',
-                        height: 30,
-                      ),
-                      const SizedBox(
-                        width: 2,
-                      ),
-                      Text(
-                        '${_profileController.myProfile.coinscount ?? 0}',
-                        style: const TextStyle(
-                          color: textColor,
-                          fontSize: 14,
-                          fontWeight: FontWeight.w600,
+                      Text('Withdrawable Balance'),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 55,
+                        decoration: BoxDecoration(
+                          color: backgroundcolorinterface,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                          child: Row(
+                            children: [
+                              Text('5720 Coins'),
+                              Text(
+                                '(\$20.39)',
+                                style: TextStyle(
+                                    fontSize: 15, fontWeight: FontWeight.bold),
+                              )
+                            ],
+                          ),
                         ),
                       ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text('Payment Method'),
+                      Container(
+                        height: 55,
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 9,
+                        ),
+                        decoration: BoxDecoration(
+                          color: backgroundcolorinterface,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Row(
+                              children: <Widget>[
+                                SvgPicture.asset('assets/svgs/coin.svg'),
+                                const SizedBox(width: 20),
+                              ],
+                            ),
+                            Expanded(
+                              child: DropdownButton<String>(
+                                value: _paymentmethods,
+                                borderRadius: BorderRadius.circular(radius),
+                                isExpanded: true,
+                                icon:
+                                    const Icon(Icons.keyboard_arrow_down_sharp),
+                                iconSize: 24,
+                                elevation: 16,
+                                underline: Container(
+                                  height: 0,
+                                  color: Colors.white,
+                                ),
+                                onChanged: (String? newValue) {
+                                  setState(() {
+                                    _paymentmethods = newValue ?? 'Paypal';
+                                  });
+                                },
+                                items: <String>[
+                                  'Paypal',
+                                  'Bank',
+                                  'Mobile Money'
+                                ].map<DropdownMenuItem<String>>((String value) {
+                                  return DropdownMenuItem<String>(
+                                    value: value,
+                                    child: Text(value),
+                                  );
+                                }).toList(),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text('Destination Address'),
+                      TextFormField(
+                        onChanged: (String val) {
+                          setState(() {});
+                        },
+                        decoration: inputDecoration.copyWith(
+                          hintText: 'Enter your wallet address',
+                          hintStyle: const TextStyle(
+                            color: iconColor,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          filled: true,
+                          fillColor: const Color(0xffF4F4F4),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      CustomButton(
+                        margin: const EdgeInsets.all(2.0),
+                        label: 'Withdraw',
+                        onPressed: () async {},
+                        buttonType: ButtonType.elevated,
+                        child: Container(),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Text(
+                        'Withdrawal History',
+                        style: TextStyle(fontWeight: FontWeight.w700),
+                      ),
+                      
                     ],
-                  )),
-              const SizedBox(
-                width: 30,
-              ),
-            ],
-          ),
-        ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  ),
+                ),
+              )
+            ];
+          },
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal:15.0),
+            child: Container(
+              child: Expanded(
+                child: Column(
                   children: [
-                    Text('Withdraw'),
-                    SvgPicture.asset('assets/svgs/help.svg')
+                    WithdrawalHeaderItem() ,
+                    Container(
+                      child: Expanded(
+                        child: ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: 10,
+                          itemBuilder: (BuildContext context, int i) {
+                            return  WithdrawalItem();
+                          },
+                        ),
+                      ),
+                    ),
                   ],
                 ),
-                Text('Withdrawable Balance'),
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                    color: backgroundcolorinterface,
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 15.0, horizontal: 15),
-                    child: Text('data'),
-                  ),
-                ),
-                Text('Payment Method'),
-                Text('Destination Address'),
-                ElevatedButton(onPressed: (){}, child: Text('Withdraw')),
-                Container(
-                  height: 200,
-                  child: ListView.builder(
-                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                      itemCount: 3,
-                      itemBuilder: (BuildContext context, int i) {
-                        return Text('data');
-                      },
-                    ),
-                ),
-              ],
+              ),
             ),
           ),
         ));
