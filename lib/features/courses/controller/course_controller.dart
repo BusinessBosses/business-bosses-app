6d185f9e188a8c12111024eafb2b8b71029b307f
Ernest Junior Awuku <awukuernest@gmail.com>
2024-04-22 16:52:07 +0000

    download materials edit

--- lib/features/courses/controller/course_controller.dart
+++ lib/features/courses/controller/course_controller.dart
@@ -14,6 +14,11 @@ class CourseController extends GetxController {
   RxBool loading = RxBool(false);
   RxBool error = RxBool(false);
   late Industry industry;
+  List<dynamic> myHistory = <dynamic>[];
+  List<dynamic> myHistoryReceived = <dynamic>[];
+  List<dynamic> myHistoryOut = <dynamic>[];
+  RxBool hLoading = RxBool(false);
+  RxBool hError = RxBool(false);
 
   @override
   void onInit() {
@@ -49,14 +54,16 @@ class CourseController extends GetxController {
   Future<void> updateCourse(Map<String, dynamic> course, String id) async {
     loading(true);
     update();
-    final ApiResponseModel response = await ApiService.put(
-        path: 'courses/update-course/$id', body: course);
+    final ApiResponseModel response =
+        await ApiService.put(path: 'courses/update-course/$id', body: course);
 
     if (response.success) {
       courses.insert(0, CourseModel.fromMap(course));
       Get.back();
       Get.snackbar('Success', 'Course updated successfully');
-    }else{print('$id');}
+    } else {
+
+    }
     loading(false);
     update();
   }
@@ -94,6 +101,7 @@ class CourseController extends GetxController {
       dynamic response = await request.send();
       if (response.statusCode == 200) {
         print('File uploaded successfully');
+        print('url of uploaded file');
         // Handle success
       } else {
         print('Error during file upload: ${response.reasonPhrase}');
--- lib/features/courses/presentation/sales.dart
+++ lib/features/courses/presentation/sales.dart
@@ -1,8 +1,10 @@
+import 'package:business_bosses_v2/common/widgets/safety_model.dart';
 import 'package:business_bosses_v2/features/courses/widgets/course_history_item.dart';
 import 'package:flutter/material.dart';
 
 class Sales extends StatefulWidget {
-  const Sales({super.key});
+    final List<dynamic> history;
+  const Sales({super.key, required this.history});
 
   @override
   // ignore: library_private_types_in_public_api
@@ -13,7 +15,13 @@ class _SalesState extends State<Sales> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: ListView.builder(
+      child: widget.history.isEmpty
+        ? const SafetyModel(
+            isLoading: false,
+            title: 'No Course Sales Found',
+            icon: Icon(Icons.warning),
+          )
+        : ListView.builder(
                 itemCount: 2,
                 itemBuilder: (BuildContext context, int i) {
                   return const CourseHistoryItem();
