8735834db584f632193fee96c261e4cf602ff377
Ernest Junior Awuku <awukuernest@gmail.com>
2024-05-19 22:23:50 +0000

    Update all_forum_screen.dart

--- lib/features/forum/presentation/all_forum_screen.dart
+++ lib/features/forum/presentation/all_forum_screen.dart
@@ -118,7 +118,7 @@ class _AllForumScreenState extends State<AllForumScreen>
                           setState(() {});
                         },
                         onSubmit: (String query) {
-                          _searchTabController.index == 0
+                          _searchTabController.index == 1
                               ? courseController.searchUsers(query)
                               : courseController.searchPosts(query);
                           setState(() {});
