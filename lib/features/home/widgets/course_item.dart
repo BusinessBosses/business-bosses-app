673f7e7a10975180b5b8219a0aead5f4ad02cc69
WadaSKID <wadaskid@gmail.com>
2024-05-21 22:37:55 +0100

    Updates

--- lib/features/home/widgets/course_item.dart
+++ lib/features/home/widgets/course_item.dart
@@ -86,7 +86,6 @@ class _CourseItemState extends State<CourseItem> {
     )
   ];
 
-  final CourseController courseController = Get.find();
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -94,8 +93,6 @@ class _CourseItemState extends State<CourseItem> {
         setState(() {
           widget.course.setViews();
         });
-        courseController.updateCourseViews(
-            widget.course.id, widget.course.views + 1);
         homeController.updateCourseViews(
             widget.course.id, widget.course.views + 1);
         Get.to(() => ExpandedCourseScreen(course: widget.course));
@@ -195,8 +192,6 @@ class _CourseItemState extends State<CourseItem> {
                         setState(() {
                           widget.course.setViews();
                         });
-                        courseController.updateCourseViews(
-                            widget.course.id, widget.course.views + 1);
                         homeController.updateCourseViews(
                             widget.course.id, widget.course.views + 1);
                         Get.to(
@@ -372,8 +367,6 @@ class _CourseItemState extends State<CourseItem> {
                                     ),
                                     TextButton(
                                       onPressed: () {
-                                        courseController
-                                            .onDeleteCourse(widget.course.id);
                                         homeController
                                             .onDeleteCourse(widget.course.id);
                                         Get.back();
@@ -649,11 +642,6 @@ class _CourseItemState extends State<CourseItem> {
               children: <Widget>[
                 TextButton.icon(
                   onPressed: () async {
-                    courseController.postLike(
-                      profileController.myProfile.uid,
-                      widget.course.id,
-                      widget.course.user!.uid,
-                    );
                     setState(() {});
                   },
                   icon: widget.course.likes
