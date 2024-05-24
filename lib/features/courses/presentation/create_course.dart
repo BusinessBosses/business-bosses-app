32b9a50e8a562661f2e581a3184f7ec2f450b4cc
Ernest Junior Awuku <awukuernest@gmail.com>
2024-05-24 11:30:54 +0000

    link ccheck update

--- lib/features/courses/presentation/create_course.dart
+++ lib/features/courses/presentation/create_course.dart
@@ -643,7 +643,8 @@ class _CreateCourseScreenState extends State<CreateCourseScreen> {
                           return;
                         }
 
-                        if (_selectedImage == null && _extractYoutubeUrls() == false) {
+                        if (_selectedImage == null &&
+                            _validateytVideoLinks() == false) {
                           Get.snackbar(
                             'Error',
                             'Please add an image for your course',
@@ -910,6 +911,16 @@ class _CreateCourseScreenState extends State<CreateCourseScreen> {
   bool _validateVideoLinks() {
     for (dynamic videoLink in videoLinks) {
       if (!_isValidUrl(videoLink.url)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  bool _validateytVideoLinks() {
+    for (dynamic videoLink in videoLinks) {
+      if (!_isValidUrl(videoLink.url) ||
+          !videoLink.url.toString().contains('youtu.be')) {
         return false;
       }
     }
--- lib/features/courses/presentation/expanded_course_screen.dart
+++ lib/features/courses/presentation/expanded_course_screen.dart
@@ -15,6 +15,7 @@ import 'package:business_bosses_v2/features/courses/widgets/downloadable_item.da
 import 'package:business_bosses_v2/features/courses/widgets/unpaidcoursepopup.dart';
 import 'package:business_bosses_v2/features/posts/presentation/boost_post_screen.dart';
 import 'package:business_bosses_v2/features/posts/widgets/youtube_display.dart';
+import 'package:business_bosses_v2/features/posts/widgets/youtube_display_item.dart';
 import 'package:business_bosses_v2/features/profile/controller/profile_controller.dart';
 import 'package:business_bosses_v2/navigation/routes.dart';
 import 'package:business_bosses_v2/utils/theme/theme.dart';
@@ -831,7 +832,7 @@ class _ExpandedCourseScreenState extends State<ExpandedCourseScreen> {
                                                         isValidYoutubeUrl(widget
                                                                 .course
                                                                 .youtubeUrls![0])
-                                                            ? YoutubeDisplay(
+                                                            ? YoutubeDisplayItem(
                                                                 widget.course
                                                                         .youtubeUrls![
                                                                     index],
