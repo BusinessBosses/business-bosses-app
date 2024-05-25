052f1780d674f7a11bffad99f149a23d8783da6d
WadaSKID <wadaskid@gmail.com>
2024-05-25 12:13:20 +0100

    Updates

--- lib/features/courses/widgets/course_item.dart
+++ lib/features/courses/widgets/course_item.dart
@@ -13,7 +13,6 @@ import 'package:business_bosses_v2/features/courses/presentation/create_course.d
 import 'package:business_bosses_v2/features/courses/presentation/expanded_course_screen.dart';
 import 'package:business_bosses_v2/features/courses/widgets/course_comment_bottomsheet.dart';
 import 'package:business_bosses_v2/features/home/controller/home_controller.dart';
-import 'package:business_bosses_v2/features/posts/widgets/youtube_display.dart';
 import 'package:business_bosses_v2/features/posts/widgets/youtube_display_item.dart';
 import 'package:business_bosses_v2/features/profile/controller/profile_controller.dart';
 import 'package:business_bosses_v2/navigation/routes.dart';
@@ -272,8 +271,7 @@ class _CourseItemState extends State<CourseItem> {
                                             onTap: () {},
                                             child: isValidYoutubeUrl(widget
                                                     .course.youtubeUrls![0])
-                                                ?
-                                                 YoutubeDisplayItem(
+                                                ? YoutubeDisplayItem(
                                                     widget.course.youtubeUrls !=
                                                             null
                                                         ? widget.course
@@ -463,8 +461,10 @@ class _CourseItemState extends State<CourseItem> {
                                             width: 3,
                                           ),
                                           Text(
-                                           widget.course.averageRating != null ? widget.course.averageRating!
-                                                .toStringAsFixed(2) : '0.00',
+                                            widget.course.averageRating != null
+                                                ? widget.course.averageRating!
+                                                    .toStringAsFixed(2)
+                                                : '0.00',
                                             style: const TextStyle(
                                               fontWeight: FontWeight.bold,
                                               fontSize: 13,
