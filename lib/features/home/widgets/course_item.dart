a3eeaa64acc41fb857565f300e2557103d7d5c0e
WadaSKID <wadaskid@gmail.com>
2024-05-25 11:04:39 +0100

    Updates

--- lib/features/home/widgets/course_item.dart
+++ lib/features/home/widgets/course_item.dart
@@ -12,7 +12,6 @@ import 'package:business_bosses_v2/features/courses/presentation/expanded_course
 import 'package:business_bosses_v2/features/courses/widgets/course_comment_bottomsheet.dart';
 import 'package:business_bosses_v2/features/home/controller/home_controller.dart';
 import 'package:business_bosses_v2/features/posts/presentation/boost_post_screen.dart';
-import 'package:business_bosses_v2/features/posts/widgets/youtube_display.dart';
 import 'package:business_bosses_v2/features/posts/widgets/youtube_display_item.dart';
 import 'package:business_bosses_v2/features/profile/controller/profile_controller.dart';
 import 'package:business_bosses_v2/navigation/routes.dart';
