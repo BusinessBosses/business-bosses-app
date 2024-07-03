a03bfe9ed6d3d579c57157180f91afa6768947d2
Ernest Junior Awuku <awukuernest@gmail.com>
2024-07-03 21:47:26 +0000

    Update suppliers_grid_tile.dart

--- lib/features/marketplace/widgets/suppliers_grid_tile.dart
+++ lib/features/marketplace/widgets/suppliers_grid_tile.dart
@@ -100,7 +100,7 @@ class _SuppliersGridTileState extends State<SuppliersGridTile> {
                     textAlign: TextAlign.center,
                     style: const TextStyle(
                         fontWeight: FontWeight.w700,
-                        fontSize: 20,
+                        fontSize: 17,
                         color: textColor),
                   ),
             Text(
@@ -110,7 +110,7 @@ class _SuppliersGridTileState extends State<SuppliersGridTile> {
               textAlign: TextAlign.center,
               style: TextStyle(
                   fontWeight: FontWeight.w700,
-                  fontSize: 17,
+                  fontSize: 15,
                   color: textColor.withAlpha(200)),
             ),
             Text(
