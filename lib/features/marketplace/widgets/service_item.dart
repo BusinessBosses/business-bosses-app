c6f6ddcf1fa3e5e95a6e6ad7e15872d65a3ac363
smartdev <smartdevs17@gmail.com>
2024-07-11 21:57:12 +0100

    add boosting to marketplace and bossup

--- lib/features/marketplace/widgets/service_item.dart
+++ lib/features/marketplace/widgets/service_item.dart
@@ -117,18 +117,18 @@ class _ServiceTileState extends State<ServiceTile> {
             style: bodyText2,
           ),
         ),
-        // if (_post.promote == false)
-        //   const PopupMenuDivider(
-        //     height: 0.0,
-        //   ),
-        // if (_post.promote == false)
-        //   const PopupMenuItem<String>(
-        //     value: 'Boost',
-        //     child: Text(
-        //       'Boost',
-        //       style: bodyText2,
-        //     ),
-        //   ),
+        if (_post.promote == false)
+          const PopupMenuDivider(
+            height: 0.0,
+          ),
+        if (_post.promote == false)
+          const PopupMenuItem<String>(
+            value: 'Boost',
+            child: Text(
+              'Boost',
+              style: bodyText2,
+            ),
+          ),
       ];
 
       final List<PopupMenuEntry<String>> myPopup = <PopupMenuEntry<String>>[
