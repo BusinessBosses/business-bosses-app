e7c63c874f36285576a8033119a074257f85adfd
Ernest Junior Awuku <awukuernest@gmail.com>
2024-07-30 16:21:18 +0000

    addition of button and progresstabbar widgets

---
+++ lib/bbpro/common/widgets/button.dart
@@ -0,0 +1,46 @@
+import 'package:business_bosses_v2/utils/theme/theme.dart';
+import 'package:flutter/material.dart';
+
+class ProCustomButton extends StatelessWidget {
+  final String text;
+  final Widget? icon;
+  final VoidCallback onPressed;
+
+  const ProCustomButton({
+    Key? key,
+    required this.text,
+    this.icon,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          primary: proprimaryColor, // Background color
+          padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              text,
+              style: TextStyle(
+                color: Colors.white, // Text color
+                fontSize: 16.0,
+              ),
+            ),
+            if (icon != null) icon!, // Display icon if it's not null
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/bbpro/common/widgets/iconbutton.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class ProIconButton extends StatelessWidget {
+  final String text;
+  final Widget? icon;
+  final VoidCallback onPressed;
+
+  const ProIconButton({
+    Key? key,
+    required this.text,
+    this.icon,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        primary: Color(0xFF4A6FA5), // Background color
+        padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          if (icon != null) icon!, // Display icon if it's not null
+          if (icon != null) SizedBox(width: 8.0), // Space between icon and text
+          Text(
+            text,
+            style: TextStyle(
+              color: Colors.white, // Text color
+              fontSize: 16.0,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
