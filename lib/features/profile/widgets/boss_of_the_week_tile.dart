901bf91a17de263b3b02012622d41edbff605d98
Ernest Junior Awuku <awukuernest@gmail.com>
2024-09-06 20:19:11 +0000

    Update boss_of_the_week_tile.dart

--- lib/features/profile/widgets/boss_of_the_week_tile.dart
+++ lib/features/profile/widgets/boss_of_the_week_tile.dart
@@ -160,147 +160,159 @@ class _BossOfWeekProfileTileState extends State<BossOfWeekProfileTile> {
                         itemCount: 2 + (homeController.bossUp?.length ?? 0),
                         itemBuilder: (context, index) {
                           if (index == 0) {
-                            return Padding(
-                              padding: const EdgeInsets.only(left: 10.0),
-                              child: Container(
-                                width: 110,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10),
-                                  color: Colors.grey[200],
-                                ),
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(10),
-                                  child: Stack(
-                                    fit: StackFit.expand,
-                                    children: [
-                                      user?.photoUrl != null
-                                          ? ImageFiltered(
-                                              imageFilter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                                              child: Image.network(
-                                                user!.photoUrl!,
-                                                fit: BoxFit.cover,
+                            return GestureDetector(
+                              onTap: () {
+                                Get.toNamed(Routes.publicProfile,
+                                    arguments: user);
+                              },
+                              child: Padding(
+                                padding: const EdgeInsets.only(left: 10.0),
+                                child: Container(
+                                  width: 110,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: Colors.grey[200],
+                                  ),
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(10),
+                                    child: Stack(
+                                      fit: StackFit.expand,
+                                      children: [
+                                        user?.photoUrl != null
+                                            ? ImageFiltered(
+                                                imageFilter: ImageFilter.blur(
+                                                    sigmaX: 10, sigmaY: 10),
+                                                child: Image.network(
+                                                  user!.photoUrl!,
+                                                  fit: BoxFit.cover,
+                                                ),
+                                              )
+                                            : ImageFiltered(
+                                                imageFilter: ImageFilter.blur(
+                                                    sigmaX: 5, sigmaY: 5),
+                                                child: Image.asset(
+                                                  'assets/images/bb_avatar.jpg',
+                                                  fit: BoxFit.cover,
+                                                ),
                                               ),
-                                            )
-                                          : ImageFiltered(
-                                              imageFilter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-                                              child: Image.asset(
-                                                'assets/images/bb_avatar.jpg',
-                                                fit: BoxFit.cover,
-                                              ),
+                                        Container(
+                                          decoration: BoxDecoration(
+                                            gradient: LinearGradient(
+                                              begin: Alignment.topCenter,
+                                              end: Alignment.bottomCenter,
+                                              colors: [
+                                                primaryColorLT.withOpacity(0.0),
+                                                primaryColorLT.withOpacity(0.6),
+                                              ],
                                             ),
-                                      Container(
-                                        decoration: BoxDecoration(
-                                          gradient: LinearGradient(
-                                            begin: Alignment.topCenter,
-                                            end: Alignment.bottomCenter,
-                                            colors: [
-                                              primaryColorLT.withOpacity(0.0),
-                                              primaryColorLT.withOpacity(0.6),
-                                            ],
                                           ),
                                         ),
-                                      ),
-                                      Positioned(
-                                        top: 5,
-                                        left: 0,
-                                        child: Container(
-                                          padding: const EdgeInsets.symmetric(
-                                              horizontal: 8, vertical: 4),
-                                          child: Row(
+                                        Positioned(
+                                          top: 5,
+                                          left: 0,
+                                          child: Container(
+                                            padding: const EdgeInsets.symmetric(
+                                                horizontal: 8, vertical: 4),
+                                            child: Row(
+                                              mainAxisSize: MainAxisSize.min,
+                                              children: [
+                                                Container(
+                                                  width: 40,
+                                                  height: 40,
+                                                  decoration: BoxDecoration(
+                                                    shape: BoxShape.circle,
+                                                    border: Border.all(
+                                                      color: Colors.white,
+                                                      width: 3,
+                                                    ),
+                                                  ),
+                                                  child: ClipOval(
+                                                    child:
+                                                        NetworkImageWithPlaceHolder(
+                                                      imageUrl:
+                                                          user?.photoUrl ?? '',
+                                                      placeHolder:
+                                                          Icons.business,
+                                                      iconSize: 25,
+                                                      fit: BoxFit.cover,
+                                                    ),
+                                                  ),
+                                                ),
+                                                const SizedBox(width: 4),
+                                                const Text(
+                                                  'Boss of \n the Week',
+                                                  maxLines: 2,
+                                                  style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 8,
+                                                    fontWeight: FontWeight.bold,
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                        ),
+                                        Positioned(
+                                          bottom: 10,
+                                          left: 10,
+                                          right: 10,
+                                          child: Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.start,
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
                                             mainAxisSize: MainAxisSize.min,
                                             children: [
-                                              Container(
-                                                width: 40,
-                                                height: 40,
-                                                decoration: BoxDecoration(
-                                                  shape: BoxShape.circle,
-                                                  border: Border.all(
-                                                    color: Colors.white,
-                                                    width: 3,
-                                                  ),
-                                                ),
-                                                child: ClipOval(
-                                                  child:
-                                                      NetworkImageWithPlaceHolder(
-                                                    imageUrl:
-                                                        user?.photoUrl ?? '',
-                                                    placeHolder: Icons.business,
-                                                    iconSize: 25,
-                                                    fit: BoxFit.cover,
-                                                  ),
-                                                ),
-                                              ),
-                                              const SizedBox(width: 4),
-                                              const Text(
-                                                'Boss of \n the Week',
-                                                maxLines: 2,
-                                                style: TextStyle(
+                                              Text(
+                                                user?.name ?? 'user?.username',
+                                                maxLines: 1,
+                                                overflow: TextOverflow.ellipsis,
+                                                style: const TextStyle(
                                                   color: Colors.white,
-                                                  fontSize: 8,
+                                                  fontSize: 12,
                                                   fontWeight: FontWeight.bold,
                                                 ),
+                                                textAlign: TextAlign.left,
                                               ),
+                                              Text(
+                                                maxLines: 1,
+                                                overflow: TextOverflow.ellipsis,
+                                                user?.bio ?? '',
+                                                style: const TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: 10,
+                                                ),
+                                                textAlign: TextAlign.left,
+                                              ),
+                                              const SizedBox(height: 5),
+                                              GestureDetector(
+                                                child: Container(
+                                                  width: double.infinity,
+                                                  padding: const EdgeInsets
+                                                      .symmetric(
+                                                      horizontal: 10,
+                                                      vertical: 8),
+                                                  decoration: BoxDecoration(
+                                                      color: Colors.white,
+                                                      borderRadius:
+                                                          BorderRadius.circular(
+                                                              5)),
+                                                  child: const Text(
+                                                    'Follow',
+                                                    style: TextStyle(
+                                                        fontSize: 12,
+                                                        color: primaryColorLT,
+                                                        fontWeight:
+                                                            FontWeight.w700),
+                                                    textAlign: TextAlign.center,
+                                                  ),
+                                                ),
+                                              )
                                             ],
                                           ),
                                         ),
-                                      ),
-                                      Positioned(
-                                        bottom: 10,
-                                        left: 10,
-                                        right: 10,
-                                        child: Column(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.start,
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
-                                          mainAxisSize: MainAxisSize.min,
-                                          children: [
-                                            Text(
-                                              user?.name ?? 'user?.username',
-                                              maxLines: 1,
-                                              overflow: TextOverflow.ellipsis,
-                                              style: const TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 12,
-                                                fontWeight: FontWeight.bold,
-                                              ),
-                                              textAlign: TextAlign.left,
-                                            ),
-                                            Text(
-                                              maxLines: 1,
-                                              overflow: TextOverflow.ellipsis,
-                                              user?.bio ?? '',
-                                              style: const TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 10,
-                                              ),
-                                              textAlign: TextAlign.left,
-                                            ),
-                                            const SizedBox(height: 5),
-                                            Container(
-                                              width: double.infinity,
-                                              padding:
-                                                  const EdgeInsets.symmetric(
-                                                      horizontal: 10,
-                                                      vertical: 8),
-                                              decoration: BoxDecoration(
-                                                  color: Colors.white,
-                                                  borderRadius:
-                                                      BorderRadius.circular(5)),
-                                              child: const Text(
-                                                'Follow',
-                                                style: TextStyle(
-                                                    fontSize: 12,
-                                                    color: primaryColorLT,
-                                                    fontWeight:
-                                                        FontWeight.w700),
-                                                textAlign: TextAlign.center,
-                                              ),
-                                            )
-                                          ],
-                                        ),
-                                      ),
-                                    ],
+                                      ],
+                                    ),
                                   ),
                                 ),
                               ),
@@ -391,8 +403,8 @@ class _BossOfWeekProfileTileState extends State<BossOfWeekProfileTile> {
                                     Padding(
                                       padding: const EdgeInsets.all(8.0),
                                       child: Container(
-                                        width: 30,
-                                        height: 30,
+                                        width: 40,
+                                        height: 40,
                                         decoration: BoxDecoration(
                                           shape: BoxShape.circle,
                                           border: Border.all(
