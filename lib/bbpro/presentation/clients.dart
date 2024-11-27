18db98ee4e39bd494de527350e6048adff21d06b
Ernest Junior Awuku <awukuernest@gmail.com>
2024-11-27 14:58:08 +0000

    Update clients.dart

--- lib/bbpro/presentation/clients.dart
+++ lib/bbpro/presentation/clients.dart
@@ -646,9 +646,13 @@ class _ClientsScreenState extends State<ClientsScreen>
         loading
             ? const SafetyModel()
             : clientsController.campaigns.isEmpty
-                ? const SafetyModel(
+                ? SafetyModel(
                     isLoading: false,
-                    icon: Icon(Icons.warning),
+                    icon: SvgPicture.asset(
+                      'assets/svgs/campaign.svg',
+                      height: 50,
+                      color: Colors.black12,
+                    ),
                     title: 'No Campaigns Yet!',
                   )
                 : ListView.builder(
