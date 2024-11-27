1f3c5d7dc111b2016500eea697a844e383d964df
wadaskid <wadaskid@gmail.com>
2024-11-27 16:38:49 +0100

    Campaign

--- lib/bbpro/widgets/campaign_item.dart
+++ lib/bbpro/widgets/campaign_item.dart
@@ -1,5 +1,4 @@
 import 'package:business_bosses_v2/bbpro/models/campaign_model.dart';
-import 'package:business_bosses_v2/bbpro/widgets/optionsbutton.dart';
 import 'package:business_bosses_v2/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
