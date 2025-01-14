5c16bfd4531753abd5acb31bbc6e00df7cfa2c0e
Ernest Junior Awuku <awukuernest@gmail.com>
2025-01-14 20:30:51 +0000

    Update create_service.dart

--- lib/bbpro/presentation/create_service.dart
+++ lib/bbpro/presentation/create_service.dart
@@ -422,14 +422,11 @@ class _CreateServiceListingState extends State<CreateServiceListing>
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: <Widget>[
-                        Text(
-                          'Add Attachment',
-                          style: TextStyle(
-                            fontSize: 13,
-                            color: Colors.black,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
+                        Text('Add Attachment',
+                            style: TextStyle(
+                                fontSize: 13,
+                                color: Colors.black,
+                                fontWeight: FontWeight.w600)),
                         Icon(Icons.image),
                       ],
                     ),
