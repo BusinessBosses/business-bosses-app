042e6ab0d274b0e2b8f91d93c13c4126fa38f1c9
wadaskid <wadaskid@gmail.com>
2025-01-15 09:31:41 +0100

    Fixes for shop loading

--- lib/bbpro/controllers/shop_controller.dart
+++ lib/bbpro/controllers/shop_controller.dart
@@ -31,6 +31,13 @@ class ShopController extends GetxController {
   ShopStats? shopStats;
   ShopGraphData? shopGraph;
 
+  
+  @override
+  void onInit() {
+    initShop();
+    super.onInit();
+  }
+
   Future<bool> initShop() async {
     ApiResponseModel response = await ApiService.get(
       path: 'shops/user-shops/${profileController.myProfile.uid}',
