2fbabc20687d38c96f8d48b57177dac3cca0c4fb
ernestjr <awukuernest@gmail.com>
2025-01-24 11:20:05 +0000

    ui change

--- lib/bbpro/presentation/shop_screen.dart
+++ lib/bbpro/presentation/shop_screen.dart
@@ -582,7 +582,7 @@ class _ShopScreenState extends State<ShopScreen> {
                                                         );
                                                       },
                                                       child: CustomItemCard(
-                                                        myShop: false,
+                                                        myShop: true,
                                                         customitem: customitem,
                                                       ),
                                                     );
