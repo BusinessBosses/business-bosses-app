9fe5c432599ebf19012457d213bb474ef25a61ab
ernestjr <awukuernest@gmail.com>
2025-02-14 15:38:41 +0000

    Update relevant_users_screen.dart

--- lib/analytics/presentation/relevant_users_screen.dart
+++ lib/analytics/presentation/relevant_users_screen.dart
@@ -129,6 +129,9 @@ class _RelevantUsersScreenState extends State<RelevantUsersScreen> {
 
   void _showFilterModal(CompleteSearchController controller) {
     showModalBottomSheet(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
         context: context,
         builder: (BuildContext context) {
           return StatefulBuilder(
