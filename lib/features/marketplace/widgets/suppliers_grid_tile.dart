49a04f308a3075d78f8972916f63bab1b2707f03
ernestjr <awukuernest@gmail.com>
2025-02-28 10:41:55 +0000

    Update suppliers_grid_tile.dart

--- lib/features/marketplace/widgets/suppliers_grid_tile.dart
+++ lib/features/marketplace/widgets/suppliers_grid_tile.dart
@@ -2,6 +2,7 @@ import 'package:business_bosses_v2/bbpro/presentation/user_shop_screen.dart';
 import 'package:business_bosses_v2/features/marketplace/models/suppliers_model.dart';
 import 'package:business_bosses_v2/features/marketplace/presentation/expandedsupplierspage.dart';
 import 'package:business_bosses_v2/features/profile/controller/profile_controller.dart';
+import 'package:business_bosses_v2/features/profile/presentation/public_profile_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -111,10 +112,12 @@ class _SuppliersGridTileState extends State<SuppliersGridTile> {
                 buttonType: ButtonType.outline,
                 onPressed: widget.supplier.isBiz!
                     ? () {
-                        Get.to(() => UserShopScreen(
-                              user: widget.supplier.user,
-                              ismyshop: false,
-                            ));
+                        Get.to(
+                          () => PublicProfileScreen(
+                            currentIndex: 1,
+                          ),
+                          arguments: widget.supplier.user,
+                        );
                       }
                     : widget.onChangeSuppliersStatus,
                 height: 36.0,
