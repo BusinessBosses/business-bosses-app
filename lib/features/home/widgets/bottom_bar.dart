4d72e0c125e9ec2d730c8d0f44b2e5139a87afe9
Ernest Awuku Junior <awukuernest@gmail.com>
2025-07-21 11:44:00 +0000

    Update bottom_bar.dart

--- lib/features/home/widgets/bottom_bar.dart
+++ lib/features/home/widgets/bottom_bar.dart
@@ -243,16 +243,11 @@ class BottomBar extends StatelessWidget {
                                                                           alignment:
                                                                               Alignment.center,
                                                                           children: <Widget>[
-                                                                            Icon(
-                                                                              Icons.star,
-                                                                              color: Colors.white,
-                                                                              size: 24,
-                                                                            ),
                                                                             Text(
                                                                               'ai',
                                                                               style: TextStyle(
-                                                                                color: Colors.black, // Choose a contrasting color
-                                                                                fontSize: 10, // Adjust size to fit within the icon
+                                                                                color: Colors.white, // Choose a contrasting color
+                                                                                fontSize: 14, // Adjust size to fit within the icon
                                                                                 fontWeight: FontWeight.bold,
                                                                               ),
                                                                             ),
