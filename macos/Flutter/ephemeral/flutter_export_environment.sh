2cc723de32086f56cd963830f32e9cb4d696aa7a
Abdullahi Wada <wadaskid@gmail.com>
2025-08-05 05:32:31 +0100

    Update Flutter version and build configuration to 2.3.4+74, adjusting environment paths for compatibility with new setup.

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\wadas\fvm\versions\stable"
+export "FLUTTER_ROOT=C:\Users\wadas\.shorebird\bin\cache\flutter\6b9522cd0097a8e5c582a0184a80b2cf6ad9ef96"
 export "FLUTTER_APPLICATION_PATH=C:\xampp\htdocs\flutter\business-bosses--v2"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=2.3.3"
-export "FLUTTER_BUILD_NUMBER=73"
+export "FLUTTER_BUILD_NAME=2.3.4"
+export "FLUTTER_BUILD_NUMBER=74"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
