b50ef444ea1d3eff118606bcfff9137a0aef9db1
Ernest Awuku Junior <awukuernest@gmail.com>
2025-08-31 13:44:30 +0000

    changes

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\wadas\fvm\versions\stable"
-export "FLUTTER_APPLICATION_PATH=C:\xampp\htdocs\flutter\business-bosses--v2"
+export "FLUTTER_ROOT=/Users/eawuku/.shorebird/bin/cache/flutter/d8361b880667126449f2be1cc170953222dce0b4"
+export "FLUTTER_APPLICATION_PATH=/Users/eawuku/Documents/GitHub/business-bosses-app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=2.3.5"
-export "FLUTTER_BUILD_NUMBER=76"
+export "FLUTTER_BUILD_NAME=2.3.6"
+export "FLUTTER_BUILD_NUMBER=78"
 export "FLUTTER_CLI_BUILD_MODE=debug"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
